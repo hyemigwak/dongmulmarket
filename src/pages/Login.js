c95a376011fbded4448a0e44d7df19dabb46ff28
unknown <goadl92@gmail.com>
2021-04-26 13:44:20 +0900

    로그인/회원가입 뷰 틀

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,40 @@
 import React from 'react'
+import styled from "styled-components";
+
 
 const Login = () => {
     return (
         <div>
-            지은님 화이팅해보아요!
+            <LoginArea>
+                <div><Input type="text" placeholder="이메일을 입력해주세요"/></div>
+                <div><Input type="password" placeholder="비밀번호를 입력해주세요"/></div>
+                <div><input type="checkbox"/><span>자동로그인</span></div>
+                <BtnArea>
+                    <button>로그인</button>
+                    <button>구글 로그인</button>
+                </BtnArea>
+                <p>회원가입</p>/<p>비밀번호 찾기</p>
+            </LoginArea>
         </div>
     )
 }
+
+const LoginArea = styled.div`
+   width: 30rem;
+   margin: 15% auto;
+   background-color: #EFEFEF;
+   border-radius: 10px;
+   text-align: center;
+   display:block;
+   
+`;
+const BtnArea = styled.div`
+    display:block;
+`;
+const Input = styled.input`
+    width: 70%;
+    height: 1.5rem;
+    margin: 0.4rem 0rem;
+`;
 
 export default Login
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,11 +1,48 @@
 import React from 'react'
+import styled from "styled-components";
 
-const Signup = () => {
+
+const Signup = (props) => {
     return (
         <div>
-            회원가입페이지입니다.
+            <SingUpArea>
+                <EmailArea>
+                    <Input type="text" placeholder="이메일을 입력해주세요"/>
+                    <button>인증</button>
+                </EmailArea>
+                <Input type="text" placeholder="인증번호를 입력해주세요"/>
+                <Input type="password" placeholder="비밀번호를 입력해주세요"/>
+                <Input type="password" placeholder="비밀번호를 다시 입력해주세요"/>
+                <button>회원가입</button>
+            </SingUpArea>
         </div>
     )
 }
+
+const SingUpArea = styled.div`
+   width: 30rem;
+   margin: 15% auto;
+   background-color: #EFEFEF;
+   border-radius: 10px;
+   text-align: center;
+   display:block;
+   
+`;
+
+const EmailArea = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Input = styled.input`
+    width: 20rem;
+    height: 1.5rem;
+    margin: 1rem 0.4rem;
+    display: inline-block;
+    
+`;
+
+
 
 export default Signup
