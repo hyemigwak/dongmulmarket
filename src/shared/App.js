18131bd1feafed5581d850912aded060cfefb6a1
unknown <goadl92@gmail.com>
2021-04-26 13:07:11 +0900

    메인화면 변경

--- src/shared/App.js
+++ src/shared/App.js
@@ -11,10 +11,10 @@ function App() {
     <React.Fragment>
        <ConnectedRouter history={history}>
         <Header/>
-        <Route exact path="/" component={Login}/>
+        <Route exact path="/login" component={Login}/>
         <Route exact path="/signup" component={Signup}/>
         <Route exact path="/mypage" component={Mypage}/>
-        <Route exact path="/main" component={Main}/>
+        <Route exact path="/" component={Main}/>
        </ConnectedRouter>
         <Footer/>
     </React.Fragment>
