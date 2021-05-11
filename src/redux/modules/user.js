50fddf3a84fedd68e79f181f02bca7784e359094
Jinnycorn <wldms949494@gmail.com>
2021-05-11 11:05:23 +0900

    0511

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -15,7 +15,7 @@ const FIND_PWD = "FIND_PWD"; //비밀번호 찾기
 const CHANGE_PWD = "CHANGE_PWD"; //비밀번호 변경
 
 //actionCreators
-const logIn = createAction(LOG_IN, (user) => ({ user }));
+const logIn = createAction(LOG_IN, (user, login_type) => ({ user,login_type}));
 const logOut = createAction(LOG_OUT, (user) => ({ user }));
 const loginCheck = createAction(LOGIN_CHECK, (cookie) => ({ cookie }));
 const getUser = createAction(GET_USER, (user) => ({ user })); // 이메일 중복체크
@@ -25,10 +25,11 @@ const changePwd = createAction(CHANGE_PWD, (user_info) => ({ user_info }));
 
 //initialState
 const initialState = {
-  user: [],
+  user: {},//딕셔너리형태
   is_login: false,
   is_exist: false, // 이메일 중복체크
   is_email_validate: false, //이메일 인증
+  login_type: "normal", //일반로그인은 normal로 설정
 };
 
 //미들웨어
@@ -41,6 +42,7 @@ const GoogleLoginAPI = (response) => {
       url: `${config.api}/account/googleAuth`,
       data: {
         // accessToken: response.accessToken,
+        // type:response.profileObj.type,
         email: response.profileObj.email,
         firstName: response.profileObj.name,
         lastName: response.profileObj.familyName ? response.profileObj.familyName : "",
@@ -50,20 +52,31 @@ const GoogleLoginAPI = (response) => {
       // },
     })
       .then((res) => {
-        console.log(res.data); // response 확인
+        console.log(res.data, "googleLogin"); // response 확인
         if (res.data.msg === "success") {
           console.log(res.data);
+          
           //토큰 받아오기
           const jwtToken = res.data.token;
           const nickname = res.data.nickname;
+          const email=res.data.email;
 
           //토큰 저장하기
           setCookie("user_token", jwtToken); //쿠키에 user_token 이라는 이름으로 저장
           setCookie("nickname", nickname);
+          setCookie("email",email);
+
+          //딕셔너리
+          const user_data={
+            email:res.data.email,
+            nickname:res.data.nickname,
+            token:res.data.token
+          }
+
 
           //디폴트로 헤더에 토큰 담아주기
           // axios.defaults.headers.common["Authorization"] = `${jwtToken}`;
-          dispatch(logIn(res.data));
+          dispatch(logIn(user_data,"google"));
 
           window.alert("정상적으로 로그인 되었습니다!");
           history.push("/");
@@ -80,6 +93,7 @@ const GoogleLoginAPI = (response) => {
 //카카오 로그인
 const kakaoLoginAPI = (response) => {
   return function (dispatch, getState, { history }) {
+    
     axios({
       method: "POST",
       url: `${config.api}/account/kakaoAuth`,
@@ -87,11 +101,12 @@ const kakaoLoginAPI = (response) => {
         email: response.profile.kakao_account.email,
         nickname: response.profile.properties.nickname,
       },
+      
     })
       .then((res) => {
         if (res.data.msg === "success") {
           console.log(res.data); // response 확인
-
+          
           //토큰 받아오기
           const jwtToken = res.data.token;
           const nickname = res.data.nickname;
@@ -100,12 +115,27 @@ const kakaoLoginAPI = (response) => {
           setCookie("user_token", jwtToken); //쿠키에 user_token 이라는 이름으로 저장
           setCookie("kakao_nickname", nickname);
 
+
+             //딕셔너리
+             const user_data={
+              email:res.data.email,
+              nickname:res.data.nickname,
+              token:res.data.token
+            }
+
+        //로그인 액션 디스패치 해주기
+        //로그인타입 넣어주기
+        dispatch(logIn(user_data,"kakao"));
+        
+
+     
           //디폴트로 헤더에 토큰 담아주기
-          axios.defaults.headers.common["Authorization"] = `${jwtToken}`;
-          dispatch(logIn(res.data));
+          // axios.defaults.headers.common["Authorization"] = `${jwtToken}`;
+          // dispatch(logIn(res.data));
 
           window.alert("정상적으로 로그인 되었습니다!");
           history.push("/");
+
         } else {
           console.log("카카오 res.data.msg fail");
         }
@@ -139,10 +169,18 @@ const loginAPI = (email, pwd) => {
           localStorage.setItem("nickname", nickname); //유저 닉네임 저장
           localStorage.setItem("email", email); //유저 이메일 저장 -> 나만 보이는 버튼에서 true/false 여부로 쓸 예정
 
+           //딕셔너리
+          const user_data={
+            email:res.data.email,
+            nickname:res.data.nickname,
+            token:res.data.token
+          }
+ 
           //디폴트로 헤더에 토큰 담아주기
-          axios.defaults.headers.common["Authorization"] = `${jwtToken}`;
+          //axios.defaults.headers.common["Authorization"] = `${jwtToken}`;
 
-          dispatch(logIn(res.data));
+          dispatch(logIn(user_data,"normal"));
+
           window.alert("정상적으로 로그인 되었습니다!");
           history.push("/");
           //자동로그아웃 -> 로그인 하자마자 1시간(토큰 만료) 되면 알럿창과 함께 로그아웃 함수 실행
@@ -151,6 +189,7 @@ const loginAPI = (email, pwd) => {
             dispatch(logOut());
             history.replace("/");
           }, 1000 * 60 * 60);
+
         } else {
           window.alert("로그인에 실패했습니다!");
         }
@@ -278,25 +317,76 @@ const ChangePwdAPI = (pwd, newPwd) => {
   };
 };
 
+//로그아웃 미들웨어
+//구글로그인하면 구글로그아웃, 카카오로그인하면 카카오로그아웃 해주기 위해서 
+// initialstate에서 정의해준 login_type를 가져옴
+//api에서 내려주는 게 없으니까 =()=> 비워둠
+
+const LogOutMiddleware=()=>{
+
+  return function(dispatch, getState, { history }){
+  
+    const loginType = getState().user.login_type;
+    console.log(loginType);
+
+  if(loginType=="normal")
+   {
+    deleteCookie("user_login");
+    deleteCookie("email");
+    deleteCookie("nickname");
+    localStorage.clear();
+    dispatch(logOut());
+
+    return;
+   }
+
+   if(loginType=="kakao")
+   {
+   
+    deleteCookie("user_token"); 
+    deleteCookie("kakao_nickname");
+    localStorage.clear();
+    dispatch(logOut());
+ 
+    return;
+   }
+   if(loginType=="google")
+   {
+     //구글닉넴따로 해야댐
+     //딜리트쿠키에
+     deleteCookie("email");
+     deleteCookie("nickname");
+    deleteCookie("G_AUTHUSER_H");
+    localStorage.clear();
+    dispatch(logOut());
+ 
+    return;
+   }
+  };
+
+};
+
 //reducer
 export default handleActions(
   {
+    
     [LOG_IN]: (state, action) =>
       produce(state, (draft) => {
-        draft.user = action.payload.user;
+        draft.user = action.payload.user;//유저가 딕셔너리
         draft.is_login = true;
+        draft.login_type=action.payload.login_type;
       }),
     [LOG_OUT]: (state, action) =>
-      produce(state, (draft) => {
-        deleteCookie("user_login");
-        deleteCookie("nickname");
-        deleteCookie("email");
-        deleteCookie("G_AUTHUSER_H");
-        deleteCookie("user_token");
-        deleteCookie("kakao_nickname");
-        localStorage.clear();
+      produce(state, (draft) => {     
+        // deleteCookie("user_login");
+        // deleteCookie("nickname");
+        // deleteCookie("email");
+        // deleteCookie("G_AUTHUSER_H");
+        // deleteCookie("user_token"); 
+        // deleteCookie("kakao_nickname");
+        // localStorage.clear();
         draft.user = null;
-        draft.is_login = false;
+        draft.is_login = true;
       }),
     [LOGIN_CHECK]: (state, action) =>
       produce(state, (draft) => {
@@ -328,6 +418,8 @@ const actionCreators = {
   EmailValidationAPI,
   FindPwdAPI,
   ChangePwdAPI,
+  LogOutMiddleware,
+
 };
 
 export { actionCreators };
