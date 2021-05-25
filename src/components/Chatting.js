2a2caf6c59f6456e21c9ce4415e754cb0efd21a8
unknown <goadl92@gmail.com>
2021-05-26 01:52:00 +0900

    2차배포수정

--- src/components/Chatting.js
+++ src/components/Chatting.js
@@ -20,7 +20,7 @@ const Chatting = memo(({ icrId, history }) => {
 
   const { chatJoinYn, handler } = useJoinChat(icrId); //참여 유무를 통해 버튼 결정
 
-  const [socket, disconnectSocket] = useSocket("http://dongmul.shop:3001/chatting", email, icrId);
+  const [socket, disconnectSocket] = useSocket("https://15.165.76.76:3001/chatting", email, icrId);
 
   useEffect(() => {
     return () => {
